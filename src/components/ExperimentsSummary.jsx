f632d763ac426dcf7f0d4ff2a79db5a1e2ac988f
Olivia Webster <oliviacweb@gmail.com>
2023-05-22 21:42:13 -0600

    create experiment summary component and styling

--- src/components/ExperimentsSummary.jsx
+++ src/components/ExperimentsSummary.jsx
@@ -1,35 +1,54 @@
-import React, {useState, useEffect} from 'react'
+
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const ExperimentsSummary = () => {
-    const [isLoaded, setIsLoaded] = useState(false)
-    const [items, setItems] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
 
-    useEffect(() => {
-        fetch('/api/summary.json', {
-            headers: {'Content-Type': 'application/json'},
-        })
-        .then(res => res.json())
-        .then(
-        (result) => {
-            setIsLoaded(true)
-            setItems(result)
+  useEffect(() => {
+    fetch('/api/summary.json', {
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          setIsLoaded(true)
+          setItems(result.items)
+          localStorage.setItem('items', JSON.stringify(result.items))
         },
-        (error) => {
-            console.log(error)
-            console.log('Error state management here.')
-        },
-        )
-    }, [],
+        error => {
+          console.log(error)
+          console.log('Error state management here.')
+        }
+      )
+  }, [])
+
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className='summary layout'>
+        <h1 className='summary__title'>All Experiments</h1>
+        <div className='summary__grid'>
+          {items.map(item => {
+            return (
+              <Link
+                to={`/experiments/${item.uuid}`}
+                key={item.uuid}
+                className='summary__card'
+              >
+                <h2 className='title'>{item.name}</h2>
+                <p className='short__name'>{item.type.short_display_name}</p>
+              </Link>
+            )
+          })}
+        </div>
+      </div>
     )
-
-    console.log(items)
-
-    if (!isLoaded) {
-        return <div>Loading...</div>
-    } else {
-        return <div>Return items here</div>
-    }
+  }
 }
-
 
 export default ExperimentsSummary
