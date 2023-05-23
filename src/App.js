bd5d7b4138d8e01d32ba02f762812cb4670c74f4
Olivia Webster <oliviacweb@gmail.com>
2023-05-22 21:32:47 -0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Home from './components/Home';
-import ExperimentsSummary from './components/ExperimentsSummary';
+import Home from './components/Home'
+import ExperimentsSummary from './components/ExperimentsSummary'
+import ExperimentDetails from './components/ExperimentDetails'
+import NotFoundPage from './components/404'
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/experiments" component={ExperimentsSummary}></Route>
+          <Route exact path='/' component={Home}></Route>
+          <Route
+            exact
+            path='/experiments'
+            component={ExperimentsSummary}
+          ></Route>
+          <Route path='/experiments/:id' component={ExperimentDetails}></Route>
+          <Route path='/404/' component={NotFoundPage}></Route>
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
